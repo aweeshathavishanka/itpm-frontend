afd7944b62780c602d3bac68529a539e393bb1bd
aweeshathavishanka <aweesha.info@gmail.com>
2025-03-19 14:22:12 +0530

    Update FAQ components to use question as AccordionItem value

--- components/shared/Sections/FAQs1.tsx
+++ components/shared/Sections/FAQs1.tsx
@@ -12,7 +12,7 @@ export default function FAQs() {
     <div className="space-y-4 pt-10">
       <Accordion type="single" collapsible className="w-full" defaultValue="3">
         {mailmindFAQs1.map((item) => (
-          <AccordionItem value={item.id} key={item.id} className="py-2">
+          <AccordionItem value={item.question} key={item.id} className="py-2">
             <AccordionPrimitive.Header className="flex">
               <AccordionPrimitive.Trigger className="focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-center gap-4 rounded-md py-5 text-left text-xl  leading-6 font-semibold transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&>svg]:-order-1 [&>svg>path:last-child]:origin-center [&>svg>path:last-child]:transition-all [&>svg>path:last-child]:duration-200 [&[data-state=open]>svg]:rotate-180 [&[data-state=open]>svg>path:last-child]:rotate-90 [&[data-state=open]>svg>path:last-child]:opacity-0 ">
                 {item.question}
